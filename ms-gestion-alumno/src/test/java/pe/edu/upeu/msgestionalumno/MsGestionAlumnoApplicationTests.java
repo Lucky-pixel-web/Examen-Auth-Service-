412fb84b16065b8ab8fb79f9bc4c2b7c306c2969
Lucky-pixel-web <luckyinever23@gmail.com>
2026-06-15 14:43:42 -0500

    fix: ms-gestion-alumno como carpeta normal

---
+++ ms-gestion-alumno/src/test/java/pe/edu/upeu/msgestionalumno/MsGestionAlumnoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.msgestionalumno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGestionAlumnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
